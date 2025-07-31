dee91ba24e6f722ad3e353e83b796e61739dac1a
Victor <curciovictor1@gmail.com>
2025-07-31 00:13:10 -0300

    canva

--- script.js
+++ script.js
@@ -10,32 +10,3 @@ function toggleMode() {
       : "assets/logo.png"
   )
 }
-
-function mostrarEmail(event) {
-  const span = document.getElementById("email-texto")
-  const email = "vcurcio.dev@gmail.com"
-
-  if (span.textContent === email) return
-
-  span.classList.add("reveal-email")
-  span.textContent = email
-
-  navigator.clipboard
-    .writeText(email)
-    .then(() => mostrarToast())
-    .catch((err) => console.error("Erro ao copiar:", err))
-
-  setTimeout(() => {
-    span.classList.remove("reveal-email")
-    span.textContent = "Email Profissional"
-  }, 3000)
-}
-
-function mostrarToast() {
-  const toast = document.getElementById("copiado-toast")
-  toast.classList.add("mostrar")
-
-  setTimeout(() => {
-    toast.classList.remove("mostrar")
-  }, 2000)
-}
